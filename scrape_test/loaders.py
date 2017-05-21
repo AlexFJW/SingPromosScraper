6967ff3df980a972c60090cf9b5b06f9f55d6e3f
Alex Fong <alexfongg@gmail.com>
2017-05-21 12:56:39 +0800

    updated gitignore

--- scrape_test/loaders.py
+++ scrape_test/loaders.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from scrapy.loader import ItemLoader
+from scrapy.loader import processors
 
-
+'''
+l.add_xpath("description", '//*[@class="hidden description"]//*[@class="value-title"][1]/@title')
+l.add_xpath("deal_start_date", '//*[contains(@class, "eventDetailsTable")]//tr[1]/td[1]//text()')
+l.add_xpath("deal_end_date", '//*[contains(@class, "eventDetailsTable")]//tr[1]/td[2]//text()')
+l.add_xpath("location", '//*[contains(@class, "eventDetailsTable")]//tr[2]/td[1]//text()')
+l.add_xpath("address", '//*[contains(@class, "eventDetailsTable")]//tr[2]/td[2]//text()')
+'''
 class DealLoader(ItemLoader):
-    '''
-    l.add_xpath("description", '//*[@class="hidden description"]//*[@class="value-title"][1]/@title')
-    l.add_xpath("deal_start_date", '//*[contains(@class, "eventDetailsTable")]//tr[1]/td[1]//text()')
-    l.add_xpath("deal_end_date", '//*[contains(@class, "eventDetailsTable")]//tr[1]/td[2]//text()')
-    l.add_xpath("location", '//*[contains(@class, "eventDetailsTable")]//tr[2]/td[1]//text()')
-    l.add_xpath("address", '//*[contains(@class, "eventDetailsTable")]//tr[2]/td[2]//text()')
-    '''
+
+    deal_end_date_in = processors.Join
+
